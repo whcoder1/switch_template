8d97207faa2cb73e9fc5b1e61652a50bc31b0f84
whcoder1 <76447937+whcoder1@users.noreply.github.com>
2022-02-02 15:19:55 +0530

    Add files via upload

---
+++ screens/Screen1.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Text,View,Button} from "react-native"
+export default class Screen1 extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>This is screen1</Text>
+            </View>
+        )
+    }
+}
---
+++ screens/Screen2.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Text,View,Button} from "react-native"
+export default class Screen2 extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>This is screen1</Text>
+            </View>
+        )
+    }
+}
